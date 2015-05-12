5701e8d8735bf0e63db1ddfa7f77d628e81c1b86
neilw22 <neilw22@gmail.com>
2015-05-12 00:44:15 -0400

    Stored stream_url in an array and generated a random index to play the appropriate item.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,38 @@
 $(document).ready(function(){
 
-SC.initialize({
-    client_id: "2dd7525162f8961f25c2b860f3fa3e25"
- 	});
+// SC.initialize({
+//     client_id: "2dd7525162f8961f25c2b860f3fa3e25"
+//  	});
 
 
 $('#play').click(function(event) {
-	$.getJSON("https://api.soundcloud.com/tracks/204620167.json?client_id=2dd7525162f8961f25c2b860f3fa3e25", function(data){
-	console.log(data.stream_url);
-	soundManager.createSound({
-  		id: 'mySound',
-  		url: data.stream_url + ".json?client_id=2dd7525162f8961f25c2b860f3fa3e25",
-  		autoLoad: true,
-  		autoPlay: true,
-  		volume: 50
-		});
-	})
+	event.preventDefault();
+	var userTags = $('#tags').val();
+	getRequest(userTags);
+
+
+	// $.getJSON("https://api.soundcloud.com/tracks/204620167.json?client_id=2dd7525162f8961f25c2b860f3fa3e25", function(data){
+	// console.log(data.stream_url);
+	// soundManager.createSound({
+ //  		id: 'mySound',
+ //  		url: data.stream_url + ".json?client_id=2dd7525162f8961f25c2b860f3fa3e25",
+ //  		autoLoad: true,
+ //  		autoPlay: true,
+ //  		volume: 50
+	// 	});
+	// })
 
 
 	
 });
 
 $('#next-track').click(function(event){
-	var userTags = $('#tags').val();
-	getRequest(userTags);
+		event.preventDefault();
+
+	soundManager.stop('mySound');
+	soundManager.destroySound('mySound');
+	// var userTags = $('#tags').val();
+	// getRequest(userTags);
 
 	// $.getJSON("https://api.soundcloud.com/tracks.json?client_id=2dd7525162f8961f25c2b860f3fa3e25", function(data){
 	// 		$.each(data, function(i, tags){
@@ -48,17 +57,19 @@ $('#next-track').click(function(event){
 });
 
 function getRequest(searchTerm){
-
+	var songList = new Array();
+	var randNumb = Math.floor((Math.random() * 49) + 1);
 	var params = {
 		client_id: '2dd7525162f8961f25c2b860f3fa3e25',
-		tags: searchTerm,
+		genres: searchTerm,
 	};
 	url = "https://api.soundcloud.com/tracks.json?";
 	$.getJSON(url, params, function(data){
 			$.each(data, function(i, songId){
-				console.log(songId.stream_url);
-				playSound(songId.stream_url);
+				songList.push(songId.stream_url);
 			});	
+			console.log(songList[randNumb]);
+			playSound(songList[randNumb]);
 		});
 	}
 
