163b110a3b244df918131d3dee48fdc5f0c81f6e
neilw22 <neilw22@gmail.com>
2015-05-12 01:23:39 -0400

    Added random image load with splash base API

--- js/app.js
+++ js/app.js
@@ -9,45 +9,14 @@ $('#play').click(function(event) {
 	event.preventDefault();
 	var userTags = $('#tags').val();
 	getRequest(userTags);
-
-
-	// $.getJSON("https://api.soundcloud.com/tracks/204620167.json?client_id=2dd7525162f8961f25c2b860f3fa3e25", function(data){
-	// console.log(data.stream_url);
-	// soundManager.createSound({
- //  		id: 'mySound',
- //  		url: data.stream_url + ".json?client_id=2dd7525162f8961f25c2b860f3fa3e25",
- //  		autoLoad: true,
- //  		autoPlay: true,
- //  		volume: 50
-	// 	});
-	// })
-
+	randImage();
 
 	
 });
 
 $('#next-track').click(function(event){
-		event.preventDefault();
-
 	soundManager.stop('mySound');
 	soundManager.destroySound('mySound');
-	// var userTags = $('#tags').val();
-	// getRequest(userTags);
-
-	// $.getJSON("https://api.soundcloud.com/tracks.json?client_id=2dd7525162f8961f25c2b860f3fa3e25", function(data){
-	// 		$.each(data, function(i, tags){
-	// 			console.log(tags.tag_list);
-	// 		});
-		  	// console.log(data[9].tag_list);
-		  	// var newTrack = (data[0].stream_url);
-		// soundManager.createSound({
-  // 		id: 'mySound',
-  // 		url: newTrack + ".json?client_id=2dd7525162f8961f25c2b860f3fa3e25",
-  // 		autoLoad: true,
-  // 		autoPlay: true,
-  // 		volume: 50
-		// });
-	// });
 
 });
 
@@ -79,7 +48,19 @@ function playSound(streamId){
   		url: streamId + ".json?client_id=2dd7525162f8961f25c2b860f3fa3e25",
   		autoLoad: true,
   		autoPlay: true,
-  		volume: 50
+  		volume: 50,
+  		onfinish: playNextSound
 		});
-		console.log(streamId);
 	}
+
+function playNextSound(){
+	playSound(songList[randNumb]);
+}
+
+function randImage() {
+	$.getJSON("http://www.splashbase.co/api/v1/images/random", "images_only=true", function(data){
+		$('body').css('background-image', 'url(' + data.url + ')');
+		console.log(data.url);
+	})
+}
+
